1c825172bf9e99e2faacc9e8c8503255af9b19e5
honwang <honglang.wang@harman.com>
2020-06-15 19:04:41 +0800

    Update T3 to 2D

--- T3.py
+++ T3.py
@@ -1,11 +1,15 @@
 import numpy as np
 from tensorflow import keras
 
-model = keras.Sequential(keras.layers.Dense(units=1, input_shape=(1,)))
+model = keras.Sequential(keras.layers.Dense(units=1, input_shape=(None, 2)))
 
-t_x = np.array([1.0, 2, 3, 4, 5, 6, 7, 8], np.float)
-l_y = np.array([2 * x - 2 for x in t_x], np.float)
+t_x = np.array([[1.0, 1.0], [2, 3], [3, 3], [4, 5], [5, 9], [6, 2], [7, 2], [8, 2]], np.float)
+l_y = np.array([], np.float)
 
-model.compile(optimizer='rmsprop', loss='mean_squared_error')
 
-model.fit(t_x, l_y, batch_size=4, epochs=5000)
+def compile():
+    model.compile(optimizer='rmsprop', loss='mean_squared_error')
+
+
+def fit():
+    model.fit(t_x, l_y, batch_size=4, epochs=5000)
