119ad53c76e236128f7194ebfbd3d186f1d3fccd
honwang <honglang.wang@harman.com>
2020-07-31 17:38:09 +0800

    Add CNN network sample

--- ocrtest.py
+++ ocrtest.py
@@ -7,7 +7,9 @@ import numpy as np
 img = cv.imread(r'e:\cv\brakeSystem_Sub.PNG')
 img_sub = img[650:690,620:740]
 
-kernel = np.ones((5,5),np.uint8)
+_ , img_sub = cv.threshold(img_sub,80,255,cv.THRESH_BINARY)
+
+kernel = np.ones((3,3),np.uint8)
 
 img_erosion = cv.erode(img_sub,kernel)
 
